17974254b2b3d1872efc23475e925052f4660e59
Iresha <nadeeshaire@gmail.com>
2021-07-21 18:14:03 +0800

    first

---
+++ material-kit-react/src/context/GlobalProvider.js
@@ -0,0 +1,3 @@
+import React ,{useReducer}  from 'react';
+import authInitialState from './initialStates/authInitialState';
+import 
---
+++ material-kit-react/src/context/initialStates/authInitialState.js
@@ -0,0 +1,6 @@
+export default{
+    isLoggedIn:flase,
+    data:{},
+    error:null,
+    loading:false,
+};
